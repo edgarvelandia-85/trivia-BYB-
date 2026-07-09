dea9d3d0022c1cad6ff4683cf12d66fa618b311a
edgarvelandia-85 <edgar.velandia@beyourbrand.com.co>
2026-07-09 08:57:14 -0500

    Update Welcome.jsx

--- src/BusinessSimulator/components/Welcome.jsx
+++ src/BusinessSimulator/components/Welcome.jsx
@@ -1,1 +1,36 @@
+import React from "react";
+import styles from "../styles";
 
+export default function Welcome({
+  player,
+  setPlayer,
+  onStart,
+}) {
+  return (
+    <div style={styles.background}>
+      <div style={styles.welcomeCard}>
+        <h1 style={styles.title}>
+          🚀 BYB BUSINESS SIMULATOR
+        </h1>
+
+        <p style={styles.subtitle}>
+          Construye la mejor estrategia utilizando los servicios de Be Your Brand.
+        </p>
+
+        <input
+          style={styles.input}
+          placeholder="Nombre del participante"
+          value={player}
+          onChange={(e) => setPlayer(e.target.value)}
+        />
+
+        <button
+          style={styles.button}
+          onClick={onStart}
+        >
+          COMENZAR
+        </button>
+      </div>
+    </div>
+  );
+}

eaf2641a06bb6b4e964e992deaad110ba2780dea
edgarvelandia-85 <edgar.velandia@beyourbrand.com.co>
2026-07-09 09:04:01 -0500

    Update ResultScreen.jsx

--- src/BusinessSimulator/components/ResultScreen.jsx
+++ src/BusinessSimulator/components/ResultScreen.jsx
@@ -1,1 +1,42 @@
+import React from "react";
+import styles from "../styles";
+import { SERVICES } from "../data";
 
+export default function ResultScreen({
+  score,
+  levels,
+  player,
+}) {
+
+  const best = SERVICES.reduce((a,b)=>
+    levels[a.id] > levels[b.id] ? a : b
+  );
+
+  return (
+    <div style={styles.background}>
+
+      <div style={styles.result}>
+
+        <h1>🏆 Simulación Finalizada</h1>
+
+        <h2>{player}</h2>
+
+        <h3>Puntaje Final</h3>
+
+        <div style={styles.finalScore}>
+          {score} pts
+        </div>
+
+        <h2 style={{marginTop:30}}>
+          Servicio recomendado
+        </h2>
+
+        <h1>
+          {best.icon} {best.name}
+        </h1>
+
+      </div>
+
+    </div>
+  );
+}

f6b6bee89ebd7cdee0d69cf54fe0e325c8f9eb67
edgarvelandia-85 <edgar.velandia@beyourbrand.com.co>
2026-07-09 08:59:28 -0500

    Update QuestionCard.jsx

--- src/BusinessSimulator/components/QuestionCard.jsx
+++ src/BusinessSimulator/components/QuestionCard.jsx
@@ -1,1 +1,41 @@
+import React from "react";
+import styles from "../styles";
 
+export default function QuestionCard({
+  question,
+  questionIndex,
+  total,
+  answer,
+}) {
+  return (
+    <div style={styles.question}>
+
+      <h2>
+        Pregunta {questionIndex + 1} de {total}
+      </h2>
+
+      <h3
+        style={{
+          marginTop:20,
+          marginBottom:30,
+          lineHeight:1.5
+        }}
+      >
+        {question.question}
+      </h3>
+
+      {question.options.map((option,index)=>(
+
+        <button
+          key={index}
+          style={styles.option}
+          onClick={()=>answer(index)}
+        >
+          {option}
+        </button>
+
+      ))}
+
+    </div>
+  );
+}
